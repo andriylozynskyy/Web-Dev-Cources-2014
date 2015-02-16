15cca472fd9a8474a5fdb20da80a120d1da0bb64
yuka <mezarecordija@gmail.com>
2015-02-16 16:30:51 +0200

    continued rewriting to gol class. not finished

--- yuka/js/game_of_life/js/game_of_life.js
+++ yuka/js/game_of_life/js/game_of_life.js
@@ -1,7 +1,7 @@
-var Game_Of_Life = function () {
+var Game_Of_Life = function (container) {
     "use strict";
 
-    var model,
+    var model, // for drawing of next step
         drawInterval;
 
     function initField () {
@@ -11,7 +11,7 @@ var Game_Of_Life = function () {
             drawInterval = null;
         }
 
-        var sideLength = 25,
+        var sideLength = 25, // the field is square
             field = document.getElementsByClassName('life__table')[0];
 
         field.style.height = (sideLength*20) + 'px';
@@ -33,6 +33,117 @@ var Game_Of_Life = function () {
             }
 
             field.appendChild(row);
+            model.push(modelRow);
         }
     }
+
+    function toggleCell(event) {
+        var element = event.target;
+
+        if (!element || !element.id || !element.classList.contains('life__cell')) {
+            return;
+        }
+
+        var coordinates = element.id.split('_'),
+            x = coordinates[1],
+            y = coordinates[2];
+
+        model[y][x] = !model[y][x];
+
+        if(element.classList.contains('alive')) {
+            element.classList.remove('alive');
+            element.classList.remove('wasalive');
+            element.classList.add('dead');
+        } else {
+            element.classList.remove('dead');
+            element.classList.add('alive');
+            element.classList.add('wasalive');
+        }
+    }
+
+    function drawStep () {
+        model.forEach(function (row, y) {
+            row.forEach(function(cell, x) {
+                var cellElement = document.querySelector('#cell_' + y + '_' + x);
+
+                if (cell) {
+                    cellElement.classList.add('alive');
+                    cellElement.classList.add('wasalive');
+                    cellElement.classList.remove('dead');
+                } else {
+                    cellElement.classList.add('dead');
+                    cellElement.classList.remove('alive');
+                }
+            });
+        })
+    }
+
+    function calculateStep () {
+        var newModel = [];
+        model.forEach(function (row, y) {
+            var newRow = [];
+
+            row.forEach(function (cell, x) {
+                newRow.push(isAlive(cell, x, y));
+            });
+            newModel.push(newRow);
+        });
+
+        model = newModel;
+        drawStep();
+    }
+
+    function endlessModel(y, x) {
+        var rx = (model[0].length + x) % model[0].length;
+        var ry = (model.length + y) % model.length;
+        return model[ry][rx];
+    }
+
+    function isAlive(wasAlive, x, y) {
+        var sum = 0;
+        sum += endlessModel(y - 1, x - 1);
+        sum += endlessModel(y, x - 1);
+        sum += endlessModel(y + 1, x - 1);
+
+        sum += endlessModel(y - 1, x);
+        sum += endlessModel(y + 1, x);
+
+        sum += endlessModel(y - 1, x + 1);
+        sum += endlessModel(y, x + 1);
+        sum += endlessModel(y + 1, x + 1);
+
+        if (wasAlive) {
+            return sum === 2 || sum === 3;
+        } else {
+            return sum === 3;
+        }
+    }
+
+    document.getElementsByClassName('life__control__button--init')[0].addEventListener('click', initField);
+
+    document.getElementsByClassName('life__control__button--pause')[0].addEventListener('click', function () {
+        if (drawInterval) {
+            clearInterval(drawInterval);
+            drawInterval = null;
+        } else {
+            drawInterval = setInterval(calculateStep, 50);
+        }
+    });
+
+    var field = document.createElement('table');
+    field.className = 'life__table';
+    container.appendChild(field);
+
+    field.addEventListener('click', toggleCell);
+    field.addEventListener('mousedown', function () {
+        field.addEventListener('mousemove', toggleCell);
+        field.addEventListener('mouseup', function () {
+            field.removeEventListener('mousemove', toggleCell);
+        });
+    });
+
+    initField();
 };
+
+var section = document.getElementsByClassName('life')[0],
+    myLife = new Game_Of_Life(section);
